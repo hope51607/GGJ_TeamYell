43d09f4b8c669e37b1d8670037d6609cf33e07f6
HaoxDev <hao0628@gmail.com>
2020-01-31 20:39:18 +0800

    Add example prj.

--- Assets/Plugins/Utopia/Scripts/Attributes/TagSelectorAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Utopia.Utilities
-{
-    public class TagSelectorAttribute : PropertyAttribute
-    {
-        public bool UseDefaultTagFieldDrawer = false;
-    }
-}
