f34e5c3a079a623c0e8be182f0699efba912e5be
NaI <fd4124983399@gmail.com>
2020-02-01 00:37:32 +0800

    吹風Template

---
+++ Assets/Scripts/Blow/BlowManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowManager : MonoSingleton<BlowManager>
+{
+    public delegate void ApplyBlowForceDelegate();
+    public ApplyBlowForceDelegate ApplyBlowForce;
+    public Transform TapeTransform;
+
+    public float BlowForce { get; private set; }
+    public Vector3 BlowDirection { get; private set; }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ApplyBlowForce?.Invoke();
+        }
+    }
+}
