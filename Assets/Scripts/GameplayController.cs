2b2906d2b69e7bc244b682c8a66702a43382be39
NaI <fd4124983399@gmail.com>
2020-02-01 23:51:58 +0800

    計時器之類的

---
+++ Assets/Scripts/GameplayController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayController : MonoSingleton<GameplayController>
+{
+    [SerializeField]
+    Text TimeText;
+
+    public BlowManager[] BlowManagers;
+
+    const int MaxTime = 60;
+    float _timeCounter;
+
+    private void Awake()
+    {
+        _timeCounter = MaxTime;
+    }
+
+    private void Update()
+    {
+        TimeText.text = ((int)_timeCounter).ToString();
+        _timeCounter -= Time.deltaTime;
+
+        if (_timeCounter <= 0)
+        {
+            TimeOut();
+        }
+    }
+
+    void TimeOut()
+    {
+        GameManager.Instance.ChangeState(GameState.Result);
+    }
+}
