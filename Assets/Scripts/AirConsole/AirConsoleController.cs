f8e21addbbf930f613b111c247a915893c0af008
hope51607 <hope51607@yahoo.com.tw>
2020-02-01 11:38:36 +0800

    Import AirConsole and add microphone handler

---
+++ Assets/Scripts/AirConsole/AirConsoleController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class AirConsoleController : MonoBehaviour {
+    private MicrophoneHandler microphoneHandler;
+    
+    void Awake()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+        microphoneHandler = GetComponent<MicrophoneHandler>();
+    }
+
+
+    void OnMessage(int from, JToken data)
+    {
+        Debug.Log("message from device " + from + ", data: " + data);
+        switch (data["action"].ToString())
+        {
+            case "motion":
+                break;
+            case "government":
+                microphoneHandler.Message(from, data);
+                break;
+            default:
+                Debug.Log(data);
+                break;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (AirConsole.instance != null)
+        {
+            AirConsole.instance.onMessage -= OnMessage;
+        }
+    }
+}
