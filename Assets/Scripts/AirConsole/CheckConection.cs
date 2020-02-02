927d25caa43afbb5cdf1bc2835a89ff43134e44a
NaI <fd4124983399@gmail.com>
2020-02-02 11:19:00 +0800

    串聯接確認

--- Assets/Scripts/AirConsole/CheckConection.cs
+++ Assets/Scripts/AirConsole/CheckConection.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheckConection : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    InputField _p1NameField, _p2NameField;
+
+    [SerializeField]
+    Button _startButton;
+
     void Awake()
     {
         AirConsole.instance.onConnect += (int deviceId) => {
             AirConsole.instance.SetActivePlayers(2);
-            AirConsole.instance.ConvertDeviceIdToPlayerNumber(deviceId);
+            int playerNumber =AirConsole.instance.ConvertDeviceIdToPlayerNumber(deviceId);
+
+            if (playerNumber == 0)
+            {
+                _p1NameField.interactable = true;
+            }
+            else if (playerNumber == 1)
+            {
+                _p2NameField.interactable = true;
+                _startButton.interactable = true;
+            }
+            else
+            {
+                Debug.Log("Player number != 0 or 1");
+            }
         };
 
         AirConsole.instance.onDisconnect += (int deviceId)=>
@@ -19,9 +40,8 @@ public class CheckConection : MonoBehaviour
         };
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGame()
     {
-        
+        GameManager.Instance.ChangeState(GameState.LoadGame);
     }
 }
